37287730ac4ae21a5d4896865f298f44da6d70f8
Florian Schwarz <77683744+FlorianSchw@users.noreply.github.com>
2022-05-13 16:26:06 +0200

    Update datashield_descriptive.R

--- R/datashield_descriptive.R
+++ R/datashield_descriptive.R
@@ -1,43 +1,30 @@
+#' DataSHIELD descriptive function
 #'
-#' @title Descriptive function for DataSHIELD
-#' @description The function summarises the outcome of variable-level aggregate DataSHIELD functions.
-#' @details datashield_descriptive functions creates summaries for all variables in a data.frame with respect to certain
-#' DataSHIELD aggregate functions providing an improved overview for a DataSHIELD analyst.
 #' @param dsfunction The function you want to run (ds.class, ds.numNA, ds.length)
 #' @param opal_connection An Opal connection
-#' @param df data.frame
 #' @param save if TRUE, the output is saved in the working directory as a csv file
+#' @param df specifies the  df that was assigned in the login, default is "D"
+#'
 #' @return A table with the output of the function, if more than one study, they are joined in one table
-#' @author Sofia Siampani (MDC), Florian Schwarz (DIfE)
 #' @export
-#'
+#' @import plyr
 #' @examples datashield_descriptive(ds.class, opals)
-#'
-
-
-datashield_descriptive <- function(dsfunction, opal_connection, df = "D", save = FALSE){
-
+datashield_descriptive<-
+  function(dsfunction, opal_connection, save = FALSE, df = "D") {
   summary <-data.frame()
   join <- list()
-
   for (p in 1:length(opal_connection)){
-
+    y <-data.frame()
     study <- opal_connection[p]
     col <- ds.colnames(df,study)
     colNames <- paste0(paste0(opal_connection[[p]]@name),".",(strsplit(as.character(substitute(dsfunction)), ".",fixed =TRUE))[[1]][2])
-
-    y <-data.frame()
-
     for(i in col[[1]]) {
-
       var <- paste0(df,"$",i)
       y[i,colNames] <- dsfunction(var, datasources= study)[1]
     }
-
     y$rn <- rownames(y)
     join[[p]]<- y
   }
-
   summary <- plyr::join_all(join, by = 'rn', type="full")
   rownames(summary) <- summary$rn
   summary$rn <-NULL
