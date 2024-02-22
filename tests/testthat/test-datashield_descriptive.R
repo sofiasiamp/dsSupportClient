ccc1abf51a5d9b071b8b29279d3aafb1711a5f6f
Sofia Siampani <sofiamaria.siampani@mdc-berlin.de>
2024-02-22 14:46:32 +0100

    second test works

--- tests/testthat/test-datashield_descriptive.R
+++ tests/testthat/test-datashield_descriptive.R
@@ -11,19 +11,19 @@ test_that("datashield_descriptive Errors", {
 
   row.names(df_comparison) <- c("ID", "Sex", "Age", "Weight")
 
-  expect_silent(case1)
+  expect_equal(case1, df_comparison)
 
-  #### Case 2: there is a problem with the coding somewhere?
-  # case2 <- datashield_descriptive(df = "D", dsfunction = ds.class, datasources = conns[c(1:4)])
-  #
-  # df_comparison2 <- data.frame(Server1.class = c("integer", "factor", "integer", "numeric"),
-  #                              Server2.class = c("integer", "factor", "integer", "numeric"),
-  #                              Server3.class = c("integer", "factor", "integer", "numeric"),
-  #                              Server4.class = c("integer", "factor", "integer", "character"))
-  #
-  # row.names(df_comparison2) <- c("ID", "Sex", "Age", "Weight")
+  ## Case 2: there is a problem with the coding somewhere?
+  case2 <- datashield_descriptive(df = "D", dsfunction = ds.class, datasources = conns[c(1:4)])
 
+  df_comparison2 <- data.frame(Server1.class = c("integer", "factor", "integer", "numeric"),
+                               Server2.class = c("integer", "factor", "integer", "numeric"),
+                               Server3.class = c("integer", "factor", "integer", "numeric"),
+                               Server4.class = c("integer", "factor", "integer", "numeric"))
 
+  row.names(df_comparison2) <- c("ID", "Sex", "Age", "Weight")
+
+  expect_equal(case2, df_comparison2)
 
 
   #case3 <- datashield_descriptive(df = "D", dsfunction = ds.numNA)
